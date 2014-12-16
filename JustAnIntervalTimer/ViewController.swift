3e6716797d413aed6a724c9c8bd7bea86bab0ab7
Geoff McGill <mcgill.geoff@gmail.com>
2014-12-16 15:01:18 -0500

    Initial Commit

---
+++ JustAnIntervalTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JustAnIntervalTimer
+//
+//  Created by McGill, Geoff on 12/16/14.
+//  Copyright (c) 2014 McGill, Geoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
